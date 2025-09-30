88e2136e0d429ae6e42b93c50ed87cecc8ebba8d
Devyn Gray <devyngray@proton.me>
2025-09-29 22:46:32 -0400

    move all config to flake

--- flake.nix
+++ flake.nix
@@ -36,7 +36,24 @@
             ./modules/nixos/configuration.nix
           ];
 
-          options.nyxed = (import ./config.nix lib).nyxedOptions;
+          options.nyxed = {
+            username = lib.mkOption {
+              type = lib.types.str;
+              default = "devyn";
+              description = "Linux username, which will have a matching group created";
+            };
+            vcs_name = lib.mkOption {
+              type = lib.types.str;
+              default = "Devyn Gray";
+              description = "Name used in jj config.toml for commits";
+            };
+            vcs_email = lib.mkOption {
+              type = lib.types.str;
+              default = "devyngray@proton.me";
+              description = "Email used in jj config.toml for commits";
+            };
+          };
+
           config.nixpkgs.config.allowUnfree = true;
         };
 
